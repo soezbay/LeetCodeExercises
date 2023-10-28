b9179f3bbf19330b44bd286c417608b95655a0e3
Safak <safak.oezbay@studmail.w-hs.de>
2023-10-28 17:21:19 +0200

    67

---
+++ src/main/java/com/mycompany/leetcode100/L67.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.leetcode100;
+
+/**
+ *
+ * @author safoz
+ */
+public class L67 {
+
+    public static String addBinary(String a, String b) {
+        
+        String shortest = a;
+        if (a.length() >= b.length())
+        {
+             shortest = b;
+        } 
+        
+        String result = "";
+        for (int i = 0; i < shortest.length(); i++)
+        {
+            
+        }
+
+        return null;
+
+    }
+}
